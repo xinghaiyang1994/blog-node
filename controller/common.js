5f73fd7b076789a4debc753bd56dc8bd87be8f27
xinghaiyang <xinghaiyang@daojia-inc.com>
2019-04-04 00:28:56 +0800

    feat 图片校验

--- controller/common.js
+++ controller/common.js
@@ -11,6 +11,15 @@ module.exports = {
     async postUpload (ctx) {
       // 获取字段为 file 的上传文件
       let file =  ctx.request.files.file
+      console.log(file)
+      // 校验图片格式
+      if (!tools.arrIn(file.type, ['image/png', 'image/jpeg', 'image/gif'])) {
+        throw new Error('只能上传 png、jpeg、jpg、gif 格式的图片！')
+      }
+      // 校验大小
+      if (file.size > 2 * 1024 * 1024) {
+        throw new Error('不能超过 2M ！')
+      }
       // 生成唯一新的文件名
       let newFileName = Date.now() + file.name 
       await fs.readFile(file.path, async (err, data) => {
@@ -23,6 +32,7 @@ module.exports = {
           }
         })  
       })
+
       ctx.body = tools.dealBody({
         data: {
           fileUrl: newFileName
--- utils/tools.js
+++ utils/tools.js
@@ -1,9 +1,17 @@
 module.exports = {
-    dealBody (option) {
-        return Object.assign({
-            code: 0,
-            message: '',
-            data: ''
-        }, option)
+  dealBody(option) {
+    return Object.assign({
+      code: 0,
+      message: '',
+      data: ''
+    }, option)
+  },
+  arrIn(value, arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return true
+      }
     }
+    return false
+  }
 }
